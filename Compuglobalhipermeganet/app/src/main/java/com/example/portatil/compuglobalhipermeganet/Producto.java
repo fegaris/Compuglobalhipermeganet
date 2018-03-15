363626fdd43661b75bead03d76c6d1998e0ee970
LaFossil <gnlaura@gmail.com>
2018-03-15 13:19:33 +0100

    laura
    clase producto

---
+++ Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/Producto.java
@@ -0,0 +1,31 @@
+package com.example.portatil.compuglobalhipermeganet;
+
+/**
+ * Created by portatil on 15/03/2018.
+ */
+
+public class Producto {
+
+        String idProducto;
+        String nombre;
+        String descripcionBreve;
+        String detalle;
+        String idFoto;
+        float precio;
+
+    Producto(String idProducto, String nombre, String descripcionBreve, String detalle, String idFoto, float precio) {
+            this.idProducto = idProducto;
+            this.nombre = nombre;
+            this.descripcionBreve = descripcionBreve;
+            this.detalle = detalle;
+            this.idFoto = idFoto;
+            this.precio = precio;
+        }
+    }
+
+
+
+
+
+
+
