7765175ffd15132ffdc27186398c3c5ee9ada563
LaFossil <gnlaura@gmail.com>
2018-03-16 10:14:52 +0100

    estructura de archivos para listado
    Layouts modificadas: lista_productos y activity_lista
    Añado Clase adaptador RVAdapter y modificar ListaActivity

--- Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/ListaActivity.java
+++ Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/ListaActivity.java
@@ -1,13 +1,43 @@
 package com.example.portatil.compuglobalhipermeganet;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaActivity extends AppCompatActivity {
+
+    Context context;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+        rv = (RecyclerView)findViewById(R.id.rv);
+        //si el tamaño no cambia, esto mejora el desempeño
+        rv.setHasFixedSize(true);
+
+        LinearLayoutManager llm = new LinearLayoutManager(context);
+        rv.setLayoutManager(llm);
+
+    }
+
+    private List<Producto> productos;
+
+
+    //String idProducto, String nombre, String descripcionBreve, String detalle, String idFoto, float precio
+    private void initializeData(){
+        productos = new ArrayList<>();
+        productos.add(new Producto("P001", "triciclo r", "triciclo rojo", "triciclo rojo de 50cm para 3 años", "triciclo.png", 55.90f));
+        productos.add(new Producto("P002", "triciclo a", "triciclo azul", "triciclo azul de 50cm para 3 años", "triciclo.png", 55.90f));
+        productos.add(new Producto("P003", "triciclo v", "triciclo verde", "triciclo verde de 50cm para 3 años", "triciclo.png", 55.90f));
+
+        RVAdapter adapter = new RVAdapter(productos);
+        rv.setAdapter(adapter);
     }
 }
---
+++ Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/RVAdapter.java
@@ -0,0 +1,66 @@
+package com.example.portatil.compuglobalhipermeganet;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by portatil on 16/03/2018.
+ */
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ProductoViewHolder>{
+
+    List<Producto> listadoProd;
+
+    RVAdapter(List<Producto> productos){
+        this.listadoProd = productos;
+    }
+
+    public static class ProductoViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView productoNombre;
+        TextView productoDescripBreve;
+        ImageView productoIdFoto;
+        TextView productoPrecio;
+
+        ProductoViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            productoNombre = (TextView)itemView.findViewById(R.id.nombre);
+            productoDescripBreve = (TextView)itemView.findViewById(R.id.descripcionBreve);
+            productoIdFoto = (ImageView)itemView.findViewById(R.id.idFoto);
+            productoPrecio = (TextView)itemView.findViewById(R.id.precio);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listadoProd.size();
+    }
+
+    @Override
+    public ProductoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.lista_productos, parent, false);
+        ProductoViewHolder pvh = new ProductoViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(ProductoViewHolder holder, int position) {
+        holder.productoNombre.setText(listadoProd.get(position).nombre);
+        holder.productoDescripBreve.setText(listadoProd.get(position).descripcionBreve);
+      //  holder.productoIdFoto.setImageResource(listadoProd.get(position).idFoto);
+      //  holder.productoPrecio.setText(listadoProd.get(position).precio);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+}
