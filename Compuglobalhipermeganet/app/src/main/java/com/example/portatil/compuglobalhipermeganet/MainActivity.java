67f1dca3113d5a56b8b8b13af6df98b0250192e2
chinrcc <rakelillacc@gmail.com>
2018-03-19 13:17:30 +0100

    raquel

--- Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/MainActivity.java
+++ Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.portatil.compuglobalhipermeganet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mostrarProductos(View view)
+    {
+        // Start the next activity
+        Intent in = new Intent().setClass(
+                MainActivity.this, ListaActivity.class);
+        in.putExtra("categoria","cat_1");
+        startActivity(in);
+
     }
 }
