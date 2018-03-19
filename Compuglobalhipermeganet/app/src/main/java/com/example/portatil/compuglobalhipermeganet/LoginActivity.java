ed7259773de03d5f61f32db6559a1b424facb884
fegaris <ismaelxe@gmail.com>
2018-03-19 19:03:58 +0100

    sincro

--- Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/LoginActivity.java
+++ Compuglobalhipermeganet/app/src/main/java/com/example/portatil/compuglobalhipermeganet/LoginActivity.java
@@ -193,6 +193,7 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("TAG", "signInWithCredential:success");
+                            Toast.makeText(LoginActivity.this, "Conectando...", Toast.LENGTH_LONG).show();
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
